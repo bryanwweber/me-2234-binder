0db4d0bc3cb3d0d04a74941d96b63a02bc63da53
Bryan W. Weber <bryan.w.weber@gmail.com>
2017-01-16 12:34:33 -0500

    Add script to install docker

---
+++ install-docker.sh
@@ -0,0 +1,9 @@
+apt install -y apt-transport-https ca-certificates linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+apt update
+apt install docker-engine
+service docker start
+curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker pull bryanwweber/thermostate
