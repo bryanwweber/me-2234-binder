56d2e0f4a3c98e1c9de4c27ad930cbccde93c345
Bryan W. Weber <bryan.w.weber@gmail.com>
2017-01-16 13:32:19 -0500

    Add iptables rule to redirect

--- install-docker.sh
+++ install-docker.sh
@@ -7,3 +7,4 @@ service docker start
 curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker pull bryanwweber/thermostate
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8000
