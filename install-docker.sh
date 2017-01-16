88559a06efb0d6e87aff9068ede317c38913acb1
Bryan W. Weber <bryan.w.weber@gmail.com>
2017-01-16 14:13:27 -0500

    Update installer script slightly

--- install-docker.sh
+++ install-docker.sh
@@ -2,9 +2,10 @@ apt install -y apt-transport-https ca-certificates linux-image-extra-$(uname -r)
 apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
 apt update
-apt install docker-engine
+apt install -y docker-engine
 service docker start
 curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 docker pull bryanwweber/thermostate
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8000
