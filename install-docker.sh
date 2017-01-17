2b022877f7e455d4e302e7663d7105cc9bd308db
Bryan W. Weber <bryan.w.weber@gmail.com>
2017-01-17 17:20:52 -0500

    Add shebang to install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 apt install -y apt-transport-https ca-certificates linux-image-extra-$(uname -r) linux-image-extra-virtual
 apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
